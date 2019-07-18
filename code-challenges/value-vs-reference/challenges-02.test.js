9454c27246b8661c38ea0b8e188fd9930585653d
ahrenswett <ahrenswett@gmail.com>
2019-07-17 20:25:34 -0700

    Value vs References Test one passed Test two passed Test three Passed Test four passed

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -56,9 +56,7 @@ setStatusAsAuthor(people);
 console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
-const setStatusAsAuthor = (people) => {
-  // Solution code here...
-};
+const setStatusAsAuthor = (people) => people.forEach(element => element.isAuthor = true);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
