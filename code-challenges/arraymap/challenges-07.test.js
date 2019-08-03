c53c4e0e8fd65880d3fe1013ea91a5f4a3c37e4e
ahrenswett <ahrenswett@gmail.com>
2019-08-03 07:17:02 -0700

    problems complete #1 #2 #3

--- code-challenges/arraymap/challenges-07.test.js
+++ code-challenges/arraymap/challenges-07.test.js
@@ -41,8 +41,8 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
-  const raised = arr.map(forEach(el =>{ el.push(Math.pow(2,el))
-  })
+  let raised = arr.map(val => Math.pow(2, val))
+  return raised//)
 };
 // const blob = dailyWeather.map(day => new Forecast(day))
 /* ------------------------------------------------------------------------------------------------
